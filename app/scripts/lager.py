165b93c4635db0f7b7ebe3e3026133fb444ac14a
Sean ONeill <seanon414@gmail.com>
2019-06-29 13:11:19 -0400

    Pulling in fridge control logic

---
+++ app/scripts/lager.py
@@ -0,0 +1,34 @@
+import logging
+import os
+import time
+from thermometer import read_temp
+
+# logging.basicConfig(filename='/home/pi/Documents/lager/lager_fridge/lager_history.log', level=logging.DEBUG, format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+
+target_temp = 45
+
+
+def should_turn_off_fridge():
+    """Checks if fridge temp is below target temp"""
+    temp_data = read_temp()[1]
+    # logging.info('Current temp is: {}, target temp is: {}'.format(temp_data, target_temp))
+    print('Current temp is: {}, target temp is: {}'.format(temp_data, target_temp))
+    return temp_data < target_temp
+
+def turn_off_fridge():
+    """Turns off fridge"""
+    # logging.info('Sending code to turn off fridge')
+    print('Sending code to turn off fridge')
+    os.system('/home/pi/rfoutlet/codesend 1054012')
+
+def turn_on_fridge():
+    """Turns on fridge"""
+    # logging.info('Sending code to turn on fridge')
+    print('Sending code to turn on fridge')
+    os.system('/home/pi/rfoutlet/codesend 1054003')
+
+if __name__ == "__main__":
+    if should_turn_off_fridge():
+        turn_off_fridge()
+    else:
+        turn_on_fridge()
--- app/views/index.py
+++ app/views/index.py
@@ -1,9 +1,24 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+from ..scripts.lager import turn_off_fridge, turn_on_fridge
 
 
 index_blueprint = Blueprint('index', __name__, static_folder='static')
 
 
-@index_blueprint.route("/")
+@index_blueprint.route("/", methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    current_temp = 60
+    target_temp = 55
+    message = ''
+    if request.method == 'POST':
+        if 'update_target' in request.form:
+            # Todo: Add ability to update target temp
+            message = 'Taget temp set to {}'.format(target_temp)
+        elif 'turn_off_fridge' in request.form:
+            turn_off_fridge
+            message = 'The frige has been turned off.'
+        elif 'turn_on_fridge' in request.form:
+            turn_on_fridge
+            message = 'The frige has been turned on.'
+
+    return render_template('index.html', current_temp=current_temp, target_temp=target_temp, message=message)
